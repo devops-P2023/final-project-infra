4c27a0feaf5be8981b3b50fdb4bf723cfb673417
Santiago Langwagen <slangwagen@gmail.com>
2023-07-27 15:01:14 -0300

    fix en provider xa ejecutar cloud

--- infra/_providers.tf
+++ infra/_providers.tf
@@ -2,5 +2,5 @@
 # Provider Block
 provider "aws" {
   region  = var.aws_region
-  profile = "default" # Defining it for default profile is Optional
+  #profile = "default" # Defining it for default profile is Optional
 }
